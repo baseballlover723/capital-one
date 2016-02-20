4c1e3fd30cd43c0f828841e798a3672c00fdc87f
Nathan Blank <blanknc@rose-hulman.edu>
2016-02-20 09:46:18 -0600

    temp

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 	
 	var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     datasets: [
         {
             label: "My First dataset",
@@ -33,7 +33,7 @@ $(document).ready(function(){
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: [65, 59, 80, 81, 56, 55, 40, 13, 100, 13, 50, 33]
         },
         {
             label: "My Second dataset",
@@ -43,7 +43,7 @@ $(document).ready(function(){
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            data: [28, 48, 40, 19, 86, 27, 90, 65, 59, 80, 81, 56]
         }
     ]
 };
