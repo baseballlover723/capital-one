f4a8e41f90a25aba727de7d2284b6928da0a068d
rosspa <rosspa@rose-hulman.edu>
2016-02-20 07:18:29 -0600

    all the mock data in being passed to the view

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,10 +1,13 @@
 class DataController < ApplicationController
   def index
-    @purchases = Purchase.all
-    @merchants = Merchant.all
-    @customers = Customer.all
     @accounts = Account.all
     @bills = Bill.all
+    @customers = Customer.all
+    @deposits = Deposit.all
+    @merchants = Merchant.all
+    @purchases = Purchase.all
+    @transfers = Transfer.all
+    @withdraws = Withdraw.all
   end
 
   def giveUserData
