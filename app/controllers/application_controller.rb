d8b74374e80c1e9cf39069d47db6954a4c897e7d
rosspa <rosspa@rose-hulman.edu>
2016-02-21 07:38:55 -0600

    break server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_numb
+
+  def set_numb
+    5 / (ENV["NUMB"] || 1).to_i
+  end
+
 end
+
---
+++ app/helpers/break_helper.rb
@@ -0,0 +1,2 @@
+module BreakHelper
+end
